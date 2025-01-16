2925898db2e0f1c59755b124fbe8b521ff9d4de0
MichaelBaker-boop <michaelbaker4@my.whccd.edu>
2025-01-15 19:58:59 -0800

    Add project files.

---
+++ COMP003A.LectureActivity1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Namespace Level //
+namespace COMP003A.LectureActivity1
+{
+    // Class Level //
+    internal class Program
+    {
+        // Main Level (Majority of Work) //
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
